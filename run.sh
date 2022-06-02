9aefdce8acf572b68c603c388ddb63210da94190
Allen050329 <68113959+Allen050329@users.noreply.github.com>
2022-06-02 08:53:43 +0800

    Update run.sh
    Add a few environment variables

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
+export FLAGS_fraction_of_gpu_memory_to_use=1.0
+export FLAGS_reallocate_gpu_memory_in_mb=200
+export FLAGS_inner_op_parallelism=8
 python3 tools/main.py --config-file configs/animeganv2.yaml 2> >(grep -v libpng) 
